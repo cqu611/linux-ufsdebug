d49deabcb82f35c0cee3c7c9a6a9b388f5ecb53d
Xxiong <xxiong@cqu.edu.cn>
2017-10-19 15:15:37 +0800

    init 4.12.3-ocufs

---
+++ drivers/scsi/lightnvm.c
@@ -0,0 +1,597 @@
+/*
+ * ufs-lightnvm.c - LightNVM UFS device
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139,
+ * USA.
+ *
+ */
+
+#include <scsi/scsi_cmnd.h>
+#include <scsi/scsi_request.h>
+#include <scsi/scsi_host.h>
+#include <scsi/scsi_device.h>
+
+#include <linux/bitops.h>
+#include <linux/lightnvm.h> 
+#include <linux/vmalloc.h>
+#include <linux/sched/sysctl.h>
+#include <uapi/linux/lightnvm.h>
+
+#include "sd.h"
+
+enum ufs_nvm_opcode {
+	UFS_NVM_ADMIN_IDENTITY		= 0xe1,
+	UFS_NVM_ADMIN_GET_L2P_TBL	= 0xe2,
+	UFS_NVM_ADMIN_SET_BB_TBL	= 0xe3,
+	UFS_NVM_ADMIN_GET_BB_TBL	= 0xe4,
+	UFS_NVM_OP_HBREAD		= 0xc1,
+	UFS_NVM_OP_HBWRITE		= 0xc2,
+	UFS_NVM_OP_PWRITE		= 0xc3,
+	UFS_NVM_OP_PREAD		= 0xc4,
+	UFS_NVM_OP_ERASE		= 0xc5,
+};
+
+#define UFS_NVM_LP_MLC_PAIRS 886
+struct ufs_nvm_l2p_mlc {
+	__le16			num_pairs;
+	__u8			pairs[UFS_NVM_LP_MLC_PAIRS];
+};
+
+struct ufs_nvm_l2p_tbl {
+	__u8			id[8];
+	struct ufs_nvm_l2p_mlc	mlc;
+};
+
+struct ufs_nvm_id_group {
+	__u8 			mtype;
+	__u8			fmtype;
+	__le16			reserved_0;
+	__u8			num_ch;
+	__u8			num_lun;
+	__u8			num_pln;
+	__u8			reserved_1;
+	__le16			num_blk;
+	__le16			num_pg;
+	__le16			fpg_sz;
+	__le16			csecs;
+	__le16			sos;
+	__le16			reserved_2;
+	__le32			trdt;
+	__le32			trdm;
+	__le32			tprt;
+	__le32			tprm;
+	__le32			tbet;
+	__le32			tbem;
+	__le32			mpos;
+	__le32			mccap;
+	__le16			cpar;
+	__u8			reserved[10];
+	struct ufs_nvm_l2p_tbl lptbl;
+} __packed;
+
+struct ufs_nvm_addr_format {
+	__u8			ch_offset;
+	__u8			ch_len;
+	__u8			lun_offset;
+	__u8			lun_len;
+	__u8			pln_offset;
+	__u8			pln_len;
+	__u8			blk_offset;
+	__u8			blk_len;
+	__u8			pg_offset;
+	__u8			pg_len;
+	__u8			sect_offset;
+	__u8			sect_len;
+	__u8			reserved[4];
+} __packed;
+
+struct ufs_nvm_id {
+	__u8			ver_id;
+	__u8			vmnt;
+	__u8			cgrps;
+	__u8			reserved_0;
+	__le32			cap;
+	__le32			dom;
+	struct ufs_nvm_addr_format ppaf;
+	__u8			reserved_1[228];
+	struct ufs_nvm_id_group groups[4];
+} __packed;
+
+struct ufs_nvm_bb_tbl {
+	__u8			tblid[4];
+	__le16			verid;
+	__le16			revid;
+	__le32			reserved_0;
+	__le32 			nr_blks;
+	__le32			nr_fact;
+	__le32			nr_grown;
+	__le32			nr_dresv;
+	__le32			nr_hresv;
+	__le32			reserved_1[8];
+	__u8			blk[0];
+
+};
+
+static inline void _ufs_nvm_check_size(void)
+{
+	BUILD_BUG_ON(sizeof(struct ufs_nvm_id_group) != 960);
+	BUILD_BUG_ON(sizeof(struct ufs_nvm_addr_format) != 16);
+	BUILD_BUG_ON(sizeof(struct ufs_nvm_id) != 4096);
+	BUILD_BUG_ON(sizeof(struct ufs_nvm_bb_tbl) != 64);
+}
+
+static int init_grps(struct nvm_id *nvmid, struct ufs_nvm_id *ufs_nvmid)
+{
+	struct ufs_nvm_id_group *src;
+	struct nvm_id_group *dst;
+
+	if (ufs_nvmid->cgrps != 1)
+		return -EINVAL;
+
+	src = &ufs_nvmid->groups[0];
+	dst = &nvmid->grp;
+
+	dst->mtype = src->mtype;
+	dst->fmtype = src->fmtype;
+	dst->num_ch = src->num_ch;
+	dst->num_lun = src->num_lun;
+	dst->num_pln = src->num_pln;
+
+	dst->num_pg = le16_to_cpu(src->num_pg);
+	dst->num_blk = le16_to_cpu(src->num_blk);
+	dst->fpg_sz = le16_to_cpu(src->fpg_sz);
+	dst->csecs = le16_to_cpu(src->csecs);
+	dst->sos = le16_to_cpu(src->sos);
+	dst->cpar = le16_to_cpu(src->cpar);
+	
+	dst->trdt = le32_to_cpu(src->trdt);
+	dst->trdm = le32_to_cpu(src->trdm);
+	dst->tprt = le32_to_cpu(src->tprt);
+	dst->tprm = le32_to_cpu(src->tprm);
+	dst->tbet = le32_to_cpu(src->tbet);
+	dst->tbem = le32_to_cpu(src->tbem);
+	dst->mpos = le32_to_cpu(src->mpos);
+	dst->mccap = le32_to_cpu(src->mccap);
+
+	if (dst->fmtype == NVM_ID_FMTYPE_MLC) {
+		memcpy(dst->lptbl.id, src->lptbl.id, 8);
+		dst->lptbl.mlc.num_pairs = le16_to_cpu(src->lptbl.mlc.num_pairs);
+		if (dst->lptbl.mlc.num_pairs > UFS_NVM_LP_MLC_PAIRS) {
+			pr_err("nvm: number of MLC pairs not supported\n");
+			return -EINVAL;
+		}
+
+		memcpy(dst->lptbl.mlc.pairs, src->lptbl.mlc.pairs, 
+				dst->lptbl.mlc.num_pairs);
+	}
+	return 0;
+}
+
+static int ufs_nvm_identity(struct nvm_dev *nvmdev, struct nvm_id *nvmid)
+{
+	struct scsi_device *sdev = nvmdev->q->queuedata;
+	struct ufs_nvm_id *ufs_nvmid;
+	struct scsi_cmnd *cmnd;
+	int ret;
+
+	ufs_nvmid = kzalloc(sizeof(struct ufs_nvm_id), GFP_KERNEL);
+	if (!ufs_nvmid)
+		return -ENOMEM;
+	cmnd = kzalloc(sizeof(struct scsi_cmnd), GFP_KERNEL);
+	if (!cmnd)
+		return -ENOMEM;
+
+
+	return ret;
+}
+
+static int ufs_nvm_get_l2p_tbl(struct nvm_dev *nvmdev, u64 slab, u32 nlb,
+				nvm_l2p_update_fn *update_l2p, void *priv)
+{
+	int ret = 0;
+
+	return ret;
+}
+
+static int ufs_nvm_get_bb_tbl(struct nvm_dev *nvmdev, struct ppa_addr ppa,
+				u8 *blks)
+{
+	int ret = 0;
+
+	return ret;
+}
+
+static int ufs_nvm_set_bb_tbl(struct nvm_dev *nvmdev, struct ppa_addr *ppas,
+				int nr_ppas, int type)
+{
+	int ret = 0;
+	
+	return ret;
+}
+
+static inline void ufs_nvm_rq2cmd(struct nvm_rq *rqd, struct scsi_device *sdev,
+				struct scsi_cmnd *cmd)
+{
+	
+}
+
+static void ufs_nvm_end_io(struct request *rq, int error) 
+{
+	struct nvm_rq *rqd = rq->end_io_data;
+
+	rqd->ppa_status = le64_to_cpu(scsi_req(rq)->result);
+	rqd->error = scsi_req(rq)->result;
+	nvm_end_io(rqd);
+
+	kfree(scsi_req(rq)->cmd);
+	blk_mq_free_request(rq);
+}
+
+static inline bool ufs_nvm_is_write(struct scsi_cmnd *cmd)
+{
+	return 0;
+}
+
+static inline struct request *ufs_nvm_alloc_request(struct request_queue *q,
+			struct scsi_cmnd *cmd)
+{
+	unsigned op = ufs_nvm_is_write(cmd) ? REQ_OP_DRV_OUT : REQ_OP_DRV_IN;
+	struct request *req;
+
+	req = blk_mq_alloc_request(q, op, 0);
+	if (IS_ERR(req))
+		return req;
+	req->cmd_flags |= REQ_FAILFAST_DRIVER;
+	// scsi_req(req)->cmd = cmd;
+
+	return req;
+}
+
+static int ufs_nvm_submit_io(struct nvm_dev *nvmdev, struct nvm_rq *rqd)
+{
+	struct request_queue *q = nvmdev->q;
+	struct scsi_device *sdev = q->queuedata;
+	struct request *rq;
+	struct bio *bio = rqd->bio;
+	struct scsi_cmnd *cmd;
+
+	cmd = kzalloc(sizeof(struct scsi_cmnd), GFP_KERNEL);
+	if (!cmd)
+		return -ENOMEM;
+
+	ufs_nvm_rq2cmd(rqd, sdev, cmd);
+
+	rq = ufs_nvm_alloc_request(q, cmd);
+	if (IS_ERR(rq)) {
+		kfree(cmd);
+		return -ENOMEM;
+	}
+	rq->cmd_flags &= ~REQ_FAILFAST_DRIVER;
+
+	if (bio) {
+		blk_init_request_from_bio(rq, bio);
+	} else {
+		rq->ioprio = IOPRIO_PRIO_VALUE(IOPRIO_CLASS_BE, IOPRIO_NORM);
+		rq->__data_len = 0;
+	}
+
+	rq->end_io_data = rqd;
+	blk_execute_rq_nowait(q, NULL, rq, 0, ufs_nvm_end_io);
+	
+	return 0;
+}
+
+static void *ufs_nvm_create_dma_pool(struct nvm_dev *nvmdev, char *name)
+{
+	struct scsi_device *scsi_dev = nvmdev->q->queuedata;
+
+	return dma_pool_create(name, &(scsi_dev->host->shost_dev), PAGE_SIZE, PAGE_SIZE, 0);
+}
+
+static void ufs_nvm_destroy_dma_pool(void *pool) 
+{
+	struct dma_pool *dma_pool = pool;
+	
+	dma_pool_destroy(dma_pool);
+}
+
+static void *ufs_nvm_dev_dma_alloc(struct nvm_dev *nvmdev, void *pool,
+				gfp_t mem_flags, dma_addr_t *dma_handler)
+{
+	return dma_pool_alloc(pool, mem_flags, dma_handler);
+}
+
+static void ufs_nvm_dev_dma_free(void * pool, void *addr,
+				dma_addr_t dma_handler)
+{
+	dma_pool_free(pool, addr, dma_handler);
+}
+
+static struct nvm_dev_ops ufs_nvm_dev_ops = {
+	.identity		= ufs_nvm_identity,
+		
+	.get_l2p_tbl	= ufs_nvm_get_l2p_tbl,
+
+	.get_bb_tbl		= ufs_nvm_get_bb_tbl,
+	.set_bb_tbl		= ufs_nvm_set_bb_tbl,
+
+	.submit_io		= ufs_nvm_submit_io,
+
+	.create_dma_pool	= ufs_nvm_create_dma_pool,
+	.destroy_dma_pool	= ufs_nvm_destroy_dma_pool,
+	.dev_dma_alloc		= ufs_nvm_dev_dma_alloc,
+	.dev_dma_free		= ufs_nvm_dev_dma_free,
+
+	.max_phys_sect		= 64,
+};
+
+static void ufs_nvm_end_user_vio(struct request *rq, int error)
+{
+	struct completion *waiting = rq->end_io_data;
+	complete(waiting);
+}
+
+static int ufs_nvm_submit_vio(struct scsi_disk *sd, struct nvm_user_vio __user *uvio)
+{
+	struct nvm_user_vio vio;
+	struct scsi_cmnd cmnd;
+	unsigned int length;
+	int ret;
+
+	if (copy_from_user(&vio, uvio, sizeof(vio)))
+		return -EFAULT;
+	if (vio.flags)
+		return -EINVAL;
+
+	return ret;
+}
+
+static int ufs_nvm_submit_user_cmd(struct request_queue *q,
+				struct scsi_disk *sd,
+				struct scsi_cmnd *cmnd,
+				void __user *ubuf, unsigned int bufflen,
+				void __user *meta_buf, unsigned int meta_len,
+				void __user *ppa_buf, unsigned int ppa_len,
+				u32 *result, u64 *status, unsigned int timeout)
+{
+	return 0;
+}
+
+static int ufs_nvm_user_vcmd(struct scsi_disk *sd, int admin,
+				struct nvm_passthru_vio __user *uvcmd)
+{
+	struct nvm_passthru_vio vcmd;
+	struct scsi_cmnd *cmnd;
+	struct request_queue *q;
+	unsigned int timeout = 0;
+	int ret;
+
+	if (copy_from_user(&vcmd, uvcmd, sizeof(vcmd)))
+		return -EFAULT;
+	if ((vcmd.opcode != 0xF2) && (!capable(CAP_SYS_ADMIN)))
+		return -EACCES;
+	if (vcmd.flags)
+		return -EINVAL;
+
+	return ret;	
+}
+
+int ufs_nvm_ioctl(struct scsi_disk *sd, unsigned int cmd, void __user *arg)
+{
+	switch (cmd) {
+	case NVME_NVM_IOCTL_ADMIN_VIO:
+		return ufs_nvm_user_vcmd(sd, 1, arg);
+	case NVME_NVM_IOCTL_IO_VIO:
+		return ufs_nvm_user_vcmd(sd, 0, arg);
+	case NVME_NVM_IOCTL_SUBMIT_VIO:
+		return ufs_nvm_submit_vio(sd, arg);
+	default:
+		return -ENOTTY;
+	}
+}
+
+int ufs_nvm_register(struct scsi_disk *sd, char *disk_name)
+{
+	struct request_queue *q = sd->device->request_queue;
+	struct nvm_dev *dev;
+
+	_ufs_nvm_check_size();
+	/* without node info, so set the node as 0 */
+	dev = kzalloc(sizeof(struct nvm_dev), GFP_KERNEL);
+	if (!dev)
+		return -ENOMEM;
+
+	dev->q = q;
+	memcpy(dev->name, disk_name, DISK_NAME_LEN);
+	dev->ops = &ufs_nvm_dev_ops;
+	dev->private_data = sd;
+	sd->nvmdev = dev;
+
+	return nvm_register(dev);
+}
+
+void ufs_nvm_unregister(struct scsi_disk *sd) 
+{
+	nvm_register(sd->nvmdev);
+}
+
+static ssize_t nvm_dev_attr_show(struct device *dev,
+				struct device_attribute *dattr, char *page)
+{
+	struct scsi_disk *sd = to_scsi_disk(dev);
+	struct nvm_dev *ndev = sd->nvmdev;
+	struct nvm_id *id;
+	struct nvm_id_group *grp;
+	struct attribute *attr;
+
+	if (!ndev)
+		return 0;
+
+	id = &ndev->identity;
+	grp = &id->grp;
+	attr = &dattr->attr;
+
+	if (strcmp(attr->name, "version") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", id->ver_id);
+	} else if (strcmp(attr->name, "vendor_opcode") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", id->vmnt);
+	} else if (strcmp(attr->name, "capabilities") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", id->cap);
+	} else if (strcmp(attr->name, "device_mode") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", id->dom);
+	/* kept for compatibility */
+	} else if (strcmp(attr->name, "media_manager") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%s\n", "gennvm");
+	} else if (strcmp(attr->name, "ppa_format") == 0) {
+		return scnprintf(page, PAGE_SIZE,
+			"0x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n",
+			id->ppaf.ch_offset, id->ppaf.ch_len,
+			id->ppaf.lun_offset, id->ppaf.lun_len,
+			id->ppaf.pln_offset, id->ppaf.pln_len,
+			id->ppaf.blk_offset, id->ppaf.blk_len,
+			id->ppaf.pg_offset, id->ppaf.pg_len,
+			id->ppaf.sect_offset, id->ppaf.sect_len);
+	} else if (strcmp(attr->name, "media_type") == 0) {	/* u8 */
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->mtype);
+	} else if (strcmp(attr->name, "flash_media_type") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->fmtype);
+	} else if (strcmp(attr->name, "num_channels") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->num_ch);
+	} else if (strcmp(attr->name, "num_luns") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->num_lun);
+	} else if (strcmp(attr->name, "num_planes") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->num_pln);
+	} else if (strcmp(attr->name, "num_blocks") == 0) {	/* u16 */
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->num_blk);
+	} else if (strcmp(attr->name, "num_pages") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->num_pg);
+	} else if (strcmp(attr->name, "page_size") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->fpg_sz);
+	} else if (strcmp(attr->name, "hw_sector_size") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->csecs);
+	} else if (strcmp(attr->name, "oob_sector_size") == 0) {/* u32 */
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->sos);
+	} else if (strcmp(attr->name, "read_typ") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->trdt);
+	} else if (strcmp(attr->name, "read_max") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->trdm);
+	} else if (strcmp(attr->name, "prog_typ") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->tprt);
+	} else if (strcmp(attr->name, "prog_max") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->tprm);
+	} else if (strcmp(attr->name, "erase_typ") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->tbet);
+	} else if (strcmp(attr->name, "erase_max") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n", grp->tbem);
+	} else if (strcmp(attr->name, "multiplane_modes") == 0) {
+		return scnprintf(page, PAGE_SIZE, "0x%08x\n", grp->mpos);
+	} else if (strcmp(attr->name, "media_capabilities") == 0) {
+		return scnprintf(page, PAGE_SIZE, "0x%08x\n", grp->mccap);
+	} else if (strcmp(attr->name, "max_phys_secs") == 0) {
+		return scnprintf(page, PAGE_SIZE, "%u\n",
+				ndev->ops->max_phys_sect);
+	} else {
+		return scnprintf(page,
+				 PAGE_SIZE,
+				 "Unhandled attr(%s) in `nvm_dev_attr_show`\n",
+				 attr->name);
+	}
+}
+
+#define NVM_DEV_ATTR_RO(_name) \
+	DEVICE_ATTR(_name, S_IRUGO, nvm_dev_attr_show, NULL)
+
+static NVM_DEV_ATTR_RO(version);
+static NVM_DEV_ATTR_RO(vendor_opcode);
+static NVM_DEV_ATTR_RO(capabilities);
+static NVM_DEV_ATTR_RO(device_mode);
+static NVM_DEV_ATTR_RO(ppa_format);
+static NVM_DEV_ATTR_RO(media_manager);
+static NVM_DEV_ATTR_RO(media_type);
+static NVM_DEV_ATTR_RO(flash_media_type);
+static NVM_DEV_ATTR_RO(num_channels);
+static NVM_DEV_ATTR_RO(num_luns);
+static NVM_DEV_ATTR_RO(num_planes);
+static NVM_DEV_ATTR_RO(num_blocks);
+static NVM_DEV_ATTR_RO(num_pages);
+static NVM_DEV_ATTR_RO(page_size);
+static NVM_DEV_ATTR_RO(hw_sector_size);
+static NVM_DEV_ATTR_RO(oob_sector_size);
+static NVM_DEV_ATTR_RO(read_typ);
+static NVM_DEV_ATTR_RO(read_max);
+static NVM_DEV_ATTR_RO(prog_typ);
+static NVM_DEV_ATTR_RO(prog_max);
+static NVM_DEV_ATTR_RO(erase_typ);
+static NVM_DEV_ATTR_RO(erase_max);
+static NVM_DEV_ATTR_RO(multiplane_modes);
+static NVM_DEV_ATTR_RO(media_capabilities);
+static NVM_DEV_ATTR_RO(max_phys_secs);
+
+static struct attribute *nvm_dev_attrs[] = {
+	&dev_attr_version.attr,
+	&dev_attr_vendor_opcode.attr,
+	&dev_attr_capabilities.attr,
+	&dev_attr_device_mode.attr,
+	&dev_attr_media_manager.attr,
+	&dev_attr_ppa_format.attr,
+	&dev_attr_media_type.attr,
+	&dev_attr_flash_media_type.attr,
+	&dev_attr_num_channels.attr,
+	&dev_attr_num_luns.attr,
+	&dev_attr_num_planes.attr,
+	&dev_attr_num_blocks.attr,
+	&dev_attr_num_pages.attr,
+	&dev_attr_page_size.attr,
+	&dev_attr_hw_sector_size.attr,
+	&dev_attr_oob_sector_size.attr,
+	&dev_attr_read_typ.attr,
+	&dev_attr_read_max.attr,
+	&dev_attr_prog_typ.attr,
+	&dev_attr_prog_max.attr,
+	&dev_attr_erase_typ.attr,
+	&dev_attr_erase_max.attr,
+	&dev_attr_multiplane_modes.attr,
+	&dev_attr_media_capabilities.attr,
+	&dev_attr_max_phys_secs.attr,
+	NULL,
+};
+
+static const struct attribute_group nvm_dev_attr_group = {
+	.name		= "lightnvm",
+	.attrs		= nvm_dev_attrs,
+};
+
+int ufs_nvm_register_sysfs(struct scsi_disk *sd)
+{
+	return sysfs_create_group(&disk_to_dev(sd->disk)->kobj, &nvm_dev_attr_group);
+}
+
+void ufs_nvm_unregister_sysfs(struct scsi_disk *sd)
+{
+	sysfs_remove_group(&disk_to_dev(sd->disk)->kobj, &nvm_dev_attr_group);
+}
+
+#define UFS_DEVICE_ID_CQU_QEMU	"qemu-test"	/* Not Used */
+#define UFS_VENDOR_CQU		   	0xeeee
+int ufs_nvm_supported(u16 vendor_id)
+{
+/*
+	if (dev_desc->wmanufacturerid == UFS_VENDOR_CQU && 
+		!strcmp(dev_desc->model, UFS_DEVICE_ID_CQU_QEMU))
+		return 1;
+*/
+	if (vendor_id == UFS_VENDOR_CQU)
+		return 1;
+	return 0;	
+}
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#7 SMP Fri Oct 13 17:40:20 CST 2017"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ae86"
+#define LINUX_COMPILER "gcc version 6.3.0 20170406 (Ubuntu 6.3.0-12ubuntu2) "
